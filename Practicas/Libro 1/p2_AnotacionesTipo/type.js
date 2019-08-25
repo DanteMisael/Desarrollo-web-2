346598fe8926352ab0c52f43021a18697dec8661
DanteMisael <fredrik098@outlook.com>
2019-08-25 11:06:12 -0600

    p2_AnotacionesTipoConError

--- Practicas/Libro 1/p2_AnotacionesTipo/type.js
+++ Practicas/Libro 1/p2_AnotacionesTipo/type.js
@@ -2,4 +2,4 @@ function area(shape, width, height) {
     var area = width * height;
     return "I'm a " + shape + " with an area of " + area + " cm squared.";
 }
-document.body.innerHTML = area("cuadro", 30, 15);
+document.body.innerHTML = area("cuadro", "treinta", 15); /*Error como pasar parametro incorrecto genera el JS, pero con error*/
--- Practicas/Libro 1/p2_AnotacionesTipo/type.ts
+++ Practicas/Libro 1/p2_AnotacionesTipo/type.ts
@@ -1,4 +1,4 @@
 function area(shape: string, width: number, height: number) {
     var area = width * height;
     return "I'm a "+ shape + " with an area of "+ area + " cm squared.";
-}document.body.innerHTML = area("cuadro", 30, 15);
+}document.body.innerHTML = area("cuadro", "treinta", 15); /*Error como pasar parametro incorrecto genera el JS, pero con error*/

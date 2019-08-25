46ddb515ec2b4ac5032b11dfdbc8d1d249b7891e
DanteMisael <fredrik098@outlook.com>
2019-08-25 10:54:34 -0600

    p2_AnotacionesTipo

---
+++ Practicas/Libro 1/p2_AnotacionesTipo/type.ts
@@ -0,0 +1,4 @@
+function area(shape: string, width: number, height: number) {
+    var area = width * height;
+    return "I'm a "+ shape + " with an area of "+ area + " cm squared.";
+}document.body.innerHTML = area("cuadro", 30, 15);

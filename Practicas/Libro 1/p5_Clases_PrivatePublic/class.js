b749e0e82f028685d786085e916724c6a5f72130
DanteMisael <fredrik098@outlook.com>
2019-08-25 14:36:38 -0600

    p5_ClaseCompleta

--- Practicas/Libro 1/p5_Clases_PrivatePublic/class.js
+++ Practicas/Libro 1/p5_Clases_PrivatePublic/class.js
@@ -1,8 +1,21 @@
 var Shape = /** @class */ (function () {
     function Shape(name, width, height) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
         this.area = width * height;
         this.color = "pink";
     }
     ;
+    Shape.prototype.shotout = function () {
+        console.log("Soy un " + name + this.color + " con un area de " + this.area + " cm cuadrados");
+        console.log("Area de la figura : " + this.area);
+        console.log("Nombre de la figura : " + name);
+        console.log("Color de la figura : " + this.color);
+        console.log("Largo de figura : " + width);
+        console.log("Ancho de figura : " + height);
+    };
     return Shape;
 }());
+var shape = new Shape("cuadro", 100, 100);
+shape.shotout();
--- Practicas/Libro 1/p5_Clases_PrivatePublic/class.ts
+++ Practicas/Libro 1/p5_Clases_PrivatePublic/class.ts
@@ -1,10 +1,20 @@
 class Shape {
-    
     area: number;
-    color: string;
+    private color: string;
 
-    constructor ( name: string, width: number, height: number ){
+    constructor ( public name: string, public width: number, public height: number ){
         this.area = width * height;
         this.color = "pink";
     };
+    shotout(){
+        console.log("Soy un " + name + this.color + " con un area de " + this.area + " cm cuadrados");
+        console.log("Area de la figura : " + this.area);
+        console.log("Nombre de la figura : " + name);
+        console.log("Color de la figura : " + this.color);
+        console.log("Largo de figura : " + width);
+        console.log("Ancho de figura : "+ height);
+    }
 }
+
+var shape = new Shape("cuadro", 100, 100);
+shape.shotout();

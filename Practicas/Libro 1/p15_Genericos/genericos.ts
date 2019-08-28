e94a47433b27ac56453c6d9f2c76e3aac751adf5
DanteMisael <fredrik098@outlook.com>
2019-08-27 20:12:17 -0600

    Genericos en general

--- Practicas/Libro 1/p15_Genericos/genericos.ts
+++ Practicas/Libro 1/p15_Genericos/genericos.ts
@@ -2,7 +2,7 @@
     Cambié el tipo de arreglo a any[] para que se pudiera 
     con todos los datos                                    */
 
-function randomElem(theArray: any[]): number {
+function randomIntElem(theArray: number[]): number {
     /*
     Math.random da un número aleatorio entre 1 y 0 de punto flotante.
     Math.floor redondea hace abajo
@@ -12,9 +12,41 @@ function randomElem(theArray: any[]): number {
     return theArray[randomIndex];
 }
 
-/*El console.log lo usé para ver que funcionan*/
+function randomStrElem(theArray: string[]): string {
+    /*
+    Math.random da un número aleatorio entre 1 y 0 de punto flotante.
+    Math.floor redondea hace abajo
+
+    La función regresa devuelve el emento del indice aleatorio*/
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}
+
+/*function randomElem(theArray: any[]): any {
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}*/
+
+function randomElem<T>(theArray: T[]): T {
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}
+
+/*El console.log lo usé para ver que funcionan
+  El primer conjunto es de numeros
+  El segundo de cadenas
+  El cuarto es para datos de cualquier tipo
+  El cuarto devuelve elementos aleatorios
 let positions: number[] = [103, 458, 472, 458];
-let randomPosition = console.log(randomElem(positions));
+let randomPosition: number = randomIntElem(positions);
 
 let colors = ['violet', 'indigo', 'blue', 'green'];
-let randomColor = console.log(randomElem(colors));
+let randomColor: string = randomStrElem(colors);
+
+let randomPosition2 = randomElem(positions);
+let randomColor2 = randomElem(colors);*/
+
+/* Si intentamos cambiar de string a numero no deja
+    Significa que genericos es mas seguro que any */
+let colors: string[] = ['violet', 'indigo', 'blue', 'green'];
+let randomColor: string = randomElem(colors);

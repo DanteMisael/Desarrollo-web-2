9a8bd6fedaa202b7a334ee980c7446ba3f31e07b
DanteMisael <fredrik098@outlook.com>
2019-08-31 20:24:34 -0600

    p4_Destructuracion arrays

--- Practicas/Libro 2/p4_Destructuracion.js
+++ Practicas/Libro 2/p4_Destructuracion.js
@@ -1,4 +1,10 @@
+// Destructuración de objetos
 var obj = { x: 1, y: 2, z: 3 };
 console.log(obj.x);
 var x = obj.x, y = obj.y, z = obj.z;
+console.log(x);
+//Destructuación de arrays
+var array = [4, 5, 6];
+console.log(array[0]);
+x = array[0], y = array[1], z = array[2];
 console.log(x);
--- Practicas/Libro 2/p4_Destructuracion.ts
+++ Practicas/Libro 2/p4_Destructuracion.ts
@@ -1,5 +1,13 @@
+// Destructuración de objetos
 var obj = {x: 1, y: 2, z: 3};
 console.log( obj.x);
 
 var {x, y, z} = obj;
+console.log(x);
+
+//Destructuación de arrays
+let array = [4, 5, 6];
+console.log(array[0]);
+
+[x, y, z] = array;
 console.log(x);

212c6b82130e1c3e30e4541e34ee4b46e907599f
DanteMisael <fredrik098@outlook.com>
2019-09-01 17:51:21 -0600

    p29 Decoradores de métodos

---
+++ Practicas/Libro 2/p29_MethodDecorator.ts
@@ -0,0 +1,14 @@
+function MethodDecorator(
+    target: Object, // The prototype of the class
+    propertyKey: string, // The name of the method
+    descriptor: TypedPropertyDescriptor<any>
+    ) {
+    console.log("MethodDecorator called on: ", target, propertyKey, descriptor);
+}
+    
+class MethodDecoratorExample {
+    @MethodDecorator
+    method() {
+    }
+}
+    

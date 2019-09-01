b8b2c950daf64fa250746afdd2e001ae5b1e0443
DanteMisael <fredrik098@outlook.com>
2019-09-01 14:03:43 -0600

    p9_jsonAsincrono

---
+++ Practicas/Libro 2/p9_jsonAsincrono.ts
@@ -0,0 +1,21 @@
+loadJSONASync('good.json')
+    .then(function (val) { console.log(val);})
+    .catch(function (err){
+        console.log('good.json error', err.message);
+    })
+
+    .then(function (){
+        return loadJSONASync('absent.json');
+    })
+    .then(function (val) {console.log(val); })
+    .catch(function (err){
+        console.log('absent.json error', err.message)
+    })
+
+    .then(function (){
+        return loadJSONASync('bad.json');
+    })
+    .then(function (val) {console.log(val); })
+    .catch(function (err) {
+        console.log('bad.json error', err.message);
+    })
